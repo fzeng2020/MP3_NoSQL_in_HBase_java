1f0cb69e9c11f6d768f3995c20d6dfd4a71b9871
damao <40133199+fzeng2020@users.noreply.github.com>
2019-03-24 14:18:55 -0400

    Update TablePartC.java

--- TablePartC.java
+++ TablePartC.java
@@ -21,11 +21,34 @@ import org.apache.hadoop.hbase.client.Scan;
 
 import org.apache.hadoop.hbase.util.Bytes;
 
-public class TablePartC{
+    public static void main(String[] args) throws IOException {
+        String line = null;
+	String fileName = "input.csv";
+	    
+        Configuration config = HBaseConfiguration.create();
+        String table_name = "powers";
+        HTable hTable = new HTable(config, table_name);
+        int i = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
 
-   public static void main(String[] args) throws IOException {
+            while ((line = reader.readLine()) != null) {
+                String[] country = line.split(",");
+                Put p = new Put(Bytes.toBytes(country[0]));
+                i++;
+                p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes(country[1]));
+                p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes(country[2]));
 
-	//TODO      
+                p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes(country[3]));
+                p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes(country[4]));
+
+                p.add(Bytes.toBytes("custom"), Bytes.toBytes("color"), Bytes.toBytes(country[5]));
+
+                hTable.put(p);
+            }
+
+        }
+
+        hTable.close();   
    }
 }
 
