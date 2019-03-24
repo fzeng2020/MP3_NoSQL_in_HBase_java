091e9b7f5b215604065133399cd0c00a9e4fd2b6
damao <40133199+fzeng2020@users.noreply.github.com>
2019-03-24 14:22:10 -0400

    Update TablePartC.java

--- TablePartC.java
+++ TablePartC.java
@@ -32,16 +32,16 @@ import org.apache.hadoop.hbase.util.Bytes;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
 
             while ((line = reader.readLine()) != null) {
-                String[] country = line.split(",");
-                Put p = new Put(Bytes.toBytes(country[0]));
+                String[] newvalue = line.split(",");
+                Put p = new Put(Bytes.toBytes(newvalue[0]));
                 i++;
-                p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes(country[1]));
-                p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes(country[2]));
+                p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes(newvalue[1]));
+                p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes(newvalue[2]));
 
-                p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes(country[3]));
-                p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes(country[4]));
+                p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes(newvalue[3]));
+                p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes(newvalue[4]));
 
-                p.add(Bytes.toBytes("custom"), Bytes.toBytes("color"), Bytes.toBytes(country[5]));
+                p.add(Bytes.toBytes("custom"), Bytes.toBytes("color"), Bytes.toBytes(newvalue[5]));
 
                 hTable.put(p);
             }
