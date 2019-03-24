6ccd8b59505be54a3a0e1b78557cc14670fed65c
damao <40133199+fzeng2020@users.noreply.github.com>
2019-03-24 10:46:56 -0400

    Add files via upload

---
+++ TablePartA.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+
+import org.apache.hadoop.hbase.TableName;
+
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class TablePartA{
+
+  //  private static final String table_name1 = "powers";
+ //   private static final String table_name2 = "food";
+    
+    public static void main(String[] args) throws IOException {
+ //initilaized HbaseAdmin
+        Configuration config = HBaseConfiguration.create();
+        HBaseAdmin admin = new HBaseAdmin(config);
+       
+       
+        String table_name1 = "powers";
+        String table_name2 = "food";
+    
+        // create new table if not exists
+        if(!admin.tableExists(table_name1)){
+            HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(table_name1));
+            tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+            tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+            tableDescriptor.addFamily(new HColumnDescriptor("custom"));
+        
+            admin.createTable(tableDescriptor);
+            System.out.println("Created table powers");
+        }
+        
+        if(!admin.tableExists(table_name2)){
+            HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(table_name2));
+            tableDescriptor.addFamily(new HColumnDescriptor("nutrition"));
+            tableDescriptor.addFamily(new HColumnDescriptor("taste"));
+            admin.createTable(tableDescriptor);
+            //TODO
+            System.out.println("Created table food");
+        }
+
+   }
+}
